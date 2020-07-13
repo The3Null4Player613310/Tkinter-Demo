9879a66b1dded8c12dbf01563a0b4fc9fe8b598a
N <nathanmunn21@gmail.com>
2020-07-13 03:53:19 -0300

    Made Basic Window
    Added code for basic window.

---
+++ Main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+
+class Test(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.setup_window()
+        self.create_widgets()
+
+    def setup_window(self):
+        self.master.title("Tkinter Test")
+        self.master.geometry("900x650+0+0")
+
+    def create_widgets(self):
+        print("wigits go herer")
+
+
+root = tk.Tk()
+test = Test(root)
+test.mainloop()
